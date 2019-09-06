2f96fcaef23ca7700c9f3e9ac6bb81d729b7685a
rensbr <49609285+rensbr@users.noreply.github.com>
2019-09-06 10:07:16 +0200

    Update MiVacuumNotificationsDZ.lua

--- MiVacuumNotificationsDZ.lua
+++ MiVacuumNotificationsDZ.lua
@@ -15,32 +15,32 @@ return {
 	},
 	execute = function(domoticz, device)
 	    --Declarations
-	    local StofzuigerState = domoticz.devices(14) -- Xiaomi Vacuum status: Charging / Paused / Cleaning / Waiting / Back to home / Sleeping / In Error
-	    local StofzuigerCont = domoticz.devices(15) -- Xiaomi Vacuum control: Clean(10) / Home(20) / Spot(30) / Pauze(40) / Stop(50) / Find(60)
-	    local StofzuigerFan = domoticz.devices(16) -- Xiaomi Vacuum Fan: Quiet(10) / Balanced(20) / Turbo(30) / Max (40)
+	    local StofzuigerState 	= domoticz.devices(14) -- Xiaomi Vacuum status: Charging / Paused / Cleaning / Waiting / Back to home / Sleeping / In Error
+	    local StofzuigerCont 	= domoticz.devices(15) -- Xiaomi Vacuum control: Clean(10) / Home(20) / Spot(30) / Pauze(40) / Stop(50) / Find(60)
+	    local StofzuigerFan 	= domoticz.devices(16) -- Xiaomi Vacuum Fan: Quiet(10) / Balanced(20) / Turbo(30) / Max (40)
 	    
 		--Function
 		print('Mi Vacuum status has changed')
 		if (StofzuigerState.rawData[1] == 'Back to home') then
-		    domoticz.notify('Mi Vacuum Cleaner','The Vacuumcleaner is going back to dock',domoticz.PRIORITY_LOW)
-		    StofzuigerFan.switchSelector(20) -- Set Fan Level to Balanced when done
+			domoticz.notify('Mi Vacuum Cleaner','The Vacuumcleaner is going back to dock',domoticz.PRIORITY_LOW)
+			StofzuigerFan.switchSelector(20) -- Set Fan Level to Balanced when done
 		end
 		
 		if (StofzuigerState.rawData[1] == 'Charging') then
-		    domoticz.notify('Mi Vacuum Cleaner','The Vacuumcleaner is docked and charging',domoticz.PRIORITY_LOW)
+		    	domoticz.notify('Mi Vacuum Cleaner','The Vacuumcleaner is docked and charging',domoticz.PRIORITY_LOW)
 		end
 		
 		if (StofzuigerState.rawData[1] == 'Cleaning') then
-		    domoticz.notify('Mi Vacuum Cleaner','The Vacuumcleaner is cleaning',domoticz.PRIORITY_LOW)
+		    	domoticz.notify('Mi Vacuum Cleaner','The Vacuumcleaner is cleaning',domoticz.PRIORITY_LOW)
 		end
 		
 		if (StofzuigerState.rawData[1] == 'In Error') then
-		    domoticz.notify('Mi Vacuum Cleaner','The Vacuumcleaner is in error',domoticz.PRIORITY_HIGH)
-            --StofzuigerCont.switchSelector(10).checkFirst() --Set Vacuum back on when in error
+		    	domoticz.notify('Mi Vacuum Cleaner','The Vacuumcleaner is in error',domoticz.PRIORITY_HIGH)
+            		--StofzuigerCont.switchSelector(10).checkFirst() --Set Vacuum back on when in error
 		end
 		
 		if (StofzuigerState.rawData[1] == 'Paused') then
-		    domoticz.notify('Mi Vacuum Cleaner','The Vacuumcleaner is paused',domoticz.PRIORITY_HIGH)
+		    	domoticz.notify('Mi Vacuum Cleaner','The Vacuumcleaner is paused',domoticz.PRIORITY_HIGH)
 		end
 	end
 }
