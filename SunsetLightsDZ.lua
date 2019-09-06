fcae934f49a4d8aa05367337e18608e353c8a068
rensbr <49609285+rensbr@users.noreply.github.com>
2019-09-06 10:10:03 +0200

    Update SunsetLightsDZ.lua

--- SunsetLightsDZ.lua
+++ SunsetLightsDZ.lua
@@ -12,31 +12,31 @@ return {
 	on = {
 		devices = {
 		    47, -- TV woonkamer
-			54, -- Iemand thuis
-			55 -- Vakantie
+		    54, -- Iemand thuis
+		    55 -- Vakantie
 		},
 		timer = {
 		    'at 15 minutes before sunset'
 		}
 	},
 	execute = function(domoticz, device)
-	    --Declarations
-		local LightWoon = domoticz.devices(139) --Lampen Woonkamer
-		local IemandThuis = domoticz.devices(54) -- Iemand Thuis
-		local Vakantie = domoticz.devices(55) -- Vakantie
-		local Weekend = domoticz.devices(74) -- Weekend
-		local TV = domoticz.devices(47) -- TV woonkamer
-		local Time = require('Time') --Time function
-        local CurrentTime = Time() --Current time
+		--Declarations
+		local LightWoon 	= domoticz.devices(139) --Lampen Woonkamer
+		local IemandThuis 	= domoticz.devices(54) -- Iemand Thuis
+		local Vakantie 		= domoticz.devices(55) -- Vakantie
+		local Weekend 		= domoticz.devices(74) -- Weekend
+		local TV 		= domoticz.devices(47) -- TV woonkamer
+		local Time 		= require('Time') --Time function
+		local CurrentTime 	= Time() --Current time
 		
 		--Function
 		if (not Vakantie.active and IemandThuis.active) then
-		    if ((CurrentTime.matchesRule('between 15 minutes before sunset and 23:14')) and (not TV.active)) then 
-		        LightWoon.setKelvin(255)
-		        LightWoon.dimTo(100)
-	        end
-	    else -- When nobody home or vacation
-            LightWoon.switchOff().checkFirst()
+			if ((CurrentTime.matchesRule('between 15 minutes before sunset and 23:14')) and (not TV.active)) then 
+		        	LightWoon.setKelvin(255)
+		        	LightWoon.dimTo(100)
+	        	end
+	    	else -- When nobody home or vacation
+            		LightWoon.switchOff().checkFirst()
 		end
 	end
 }
