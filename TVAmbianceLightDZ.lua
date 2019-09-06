8f50538129dad5f04c88e8f5a170eed6f350a071
rensbr <49609285+rensbr@users.noreply.github.com>
2019-09-06 10:11:07 +0200

    Update TVAmbianceLightDZ.lua

--- TVAmbianceLightDZ.lua
+++ TVAmbianceLightDZ.lua
@@ -10,7 +10,7 @@
 return {
 	on = {
 		devices = {
-			47 -- TV woonkamer
+		    47 -- TV woonkamer
 		},
 		timer = {
 		    'at 15 minutes before sunset'
@@ -18,30 +18,30 @@ return {
 	},
 	execute = function(domoticz, device)
 	    --Declarations
-		local LightWoon = domoticz.devices(139) --Lampen Woonkamer
-		local LightTV = domoticz.devices(121) --Lampen TV woonkamer
-		local TV = domoticz.devices(47) -- TV woonkamer
-		local Time = require('Time') --Time function
-    local CurrentTime = Time() --Current time
+		local LightWoon 	= domoticz.devices(139) --Lampen Woonkamer
+		local LightTV 		= domoticz.devices(121) --Lampen TV woonkamer
+		local TV 		= domoticz.devices(47) -- TV woonkamer
+		local Time 		= require('Time') --Time function
+    		local CurrentTime 	= Time() --Current time
 		
 		--Function
 		if ((TV.active) and (CurrentTime.matchesRule('at 15 minutes before sunset-sunrise'))) then -- When sunset-sunrise and TV is on
-		    --Set color and dim is not possible with standard function, so that has to be arranged with the direct link
-		    domoticz.openURL({ --Light Woonkamer
-                url = domoticz.settings['Domoticz url'] .. '/json.htm?type=command&param=setcolbrightnessvalue&idx=139&color={"m":3,"t":0,"r":255,"g":204,"b":43,"cw":0,"ww":0}&brightness=90',
-                method = "GET",
-            })
+		    	--Set color and dim is not possible with standard function, so that has to be arranged with the direct link
+		    	domoticz.openURL({ --Light Woonkamer
+                	url = domoticz.settings['Domoticz url'] .. '/json.htm?type=command&param=setcolbrightnessvalue&idx=139&color={"m":3,"t":0,"r":255,"g":204,"b":43,"cw":0,"ww":0}&brightness=90',
+                	method = "GET",
+            		})
             
-            domoticz.openURL({ -- Light TV
-                url = domoticz.settings['Domoticz url'] .. '/json.htm?type=command&param=setcolbrightnessvalue&idx=121&color={"m":3,"t":0,"r":255,"g":204,"b":43,"cw":0,"ww":0}&brightness=90',
-                method = "GET",
-            })
+            		domoticz.openURL({ -- Light TV
+                	url = domoticz.settings['Domoticz url'] .. '/json.htm?type=command&param=setcolbrightnessvalue&idx=121&color={"m":3,"t":0,"r":255,"g":204,"b":43,"cw":0,"ww":0}&brightness=90',
+                	method = "GET",
+            		})
 		elseif (not TV.active) then -- When tV off 
-		    if (LightWoon.active) then -- lamp is still on
-		        LightWoon.setKelvin(255)
-		        LightWoon.dimTo(100)
-		    end
-		    LightTV.switchOff().checkFirst()
+		    	if (LightWoon.active) then -- lamp is still on
+		        	LightWoon.setKelvin(255)
+		        	LightWoon.dimTo(100)
+		    	end
+		    	LightTV.switchOff().checkFirst()
 		end    
 	end
 }
