094785f019291da0757fcf9737dd18873802ef99
rensbr <49609285+rensbr@users.noreply.github.com>
2022-10-05 13:26:13 +0200

    Update SomebodyHome.lua
    Added override and delay

--- SomebodyHome.lua
+++ SomebodyHome.lua
@@ -1,31 +1,51 @@
 --*------------------------------------------------------------------------------------*--
 --Creator: Rens
 --Date: 26-04-2019
---Last update: 26-04-2019
---Changelog:
+--Last update: 13-09-2022
+--Changelog: 
 --Source: -
---Description: Used check whenevery anyone is home depending on ping
+--Description: Used check whenevery anyone is home with delay
 --*------------------------------------------------------------------------------------*--
+
+--Declarations
+local Person1Num    = 1 -- Person 
+local Person2Num    = 2 -- Person 
+local Person3Num    = 3 -- Person
+local OverrideNum   = 4 -- Person     
+local Person4Num    = 5 -- Person
+local Person5Num    = 6 -- Person
 
 return {
 	on = {
 		devices = {
-			52, -- Person one
-			53  -- Person two
+			Person1Num,	-- Person
+			Person2Num,     -- Person
+			Person3Num,     -- Person
+			Person4Num,     -- Person
+			Person5Num,     -- Person
+			OverrideNum     -- Override  
 		}
 	},
 	execute = function(domoticz, device)
-		 --Declarations
-		local IemandThuis 	= domoticz.devices(54) -- Iemand Thuis
-		local One 		= domoticz.devices(52) -- Person one 
-		local Two 		= domoticz.devices(53) -- Person two
-		
-        --Function
-        if (One.active) or (Two.active) then -- When somebody is at home
-            IemandThuis.switchOn().checkFirst()
-        else
-            IemandThuis.switchOff().checkFirst()
-        end
-        
+		--Declarations
+		local IemandThuis	= domoticz.devices(54) -- Iemand Thuis
+		local Person1       	= domoticz.devices(Person1Num) -- Person 
+		local Person2         	= domoticz.devices(Person2Num) -- Person 
+		local Person3       	= domoticz.devices(Person3Num) -- Person
+		local Person4         	= domoticz.devices(Person4Num) -- Person
+		local Person5           = domoticz.devices(Person5Num) -- Person
+		local Override      	= domoticz.devices(OverrideNum) -- Override 
+
+		--Function
+		if (Person1.active) or (Person2.active) or (Person3.active) or (Person4.active) or (Person5.active) or (Override.active) then -- When somebody is home
+		    IemandThuis.cancelQueuedCommands()
+		    IemandThuis.switchOn().checkFirst()
+		else
+		    IemandThuis.switchOff().afterMin(10)
+		end
+
+		if ((Person1.active) or (Person2.active) or (Person3.active) or (Person4.active) or (Person5.active)) and (Override.active) then -- Turn off override when real precense is there
+		    Override.switchOff().checkFirst()
+		end
 	end
 }
